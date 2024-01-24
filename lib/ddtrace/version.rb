5ede092ab7b16b9a2e9bd0aa72a905e8496f0502
Ivo Anjo <ivo.anjo@datadoghq.com>
2024-01-24 14:45:48 +0000

    Add custom build version to easily distinguish version

--- lib/ddtrace/version.rb
+++ lib/ddtrace/version.rb
@@ -5,8 +5,8 @@ module DDTrace
     MAJOR = 1
     MINOR = 19
     PATCH = 0
-    PRE = nil
-    BUILD = nil
+    PRE = 1
+    BUILD = 'timeline0'
     # PRE and BUILD above are modified for dev gems during gem build GHA workflow
 
     STRING = [MAJOR, MINOR, PATCH, PRE, BUILD].compact.join('.')
